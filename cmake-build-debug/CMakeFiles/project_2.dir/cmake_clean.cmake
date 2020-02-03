b68b71c0e2f760a3e316e5340376162ab788e310
elkhoe <ekhoe.bg@gmail.com>
2020-02-03 01:33:19 -0800

    initial files

---
+++ cmake-build-debug/CMakeFiles/project_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_2.dir/main.cpp.o"
+  "project_2.pdb"
+  "project_2.exe"
+  "libproject_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
